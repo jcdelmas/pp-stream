bf9a2ff489ad060140199188d924bedc3b9afe9e
Jean-Christophe Delmas <jc.delmas@gmail.com>
2015-08-07 00:38:55 +0200

    add pipe class

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var Promise = require("bluebird");
 var _ = require("lodash");
 
 function Stream() {
+  this.closed = false
 };
 
+Stream.prototype.wrapper = StreamWrapper;
+
+Stream.prototype.close = function() {
+  ths.closed = true;
+}
+
 Stream.prototype.map = function(fn) {
-  var self = this
-  return new DefaultStream(function() {
-    return self.next().then(function(item) {
+  var self = this;
+  return new self.wrapper(self, function() {
+    return this.wrapped.next().then(function(item) {
       return !item.done ? { value: fn(item.value), done: false } : item;
     });
   });
@@ -18,15 +25,31 @@ Stream.prototype.filter = function(fn) {
   var self = this;
 
   function _filter() {
-    return self.next().then(function(item) {
+    var self = this;
+    return self.wrapped.next().then(function(item) {
       if (item.done || fn(item.value)) {
         return item;
       } else {
-        return _filter();
+        return self.next();
       }
     });
   };
-  return new DefaultStream(_filter);
+  return new self.wrapper(self, _filter);
+};
+
+Stream.prototype.pipe = function(pipe) {
+  var self = this;
+  (function _consume() {
+    pipe.ready().then(function(consumer) {
+      if (!consumer.done) {
+        self.next().then(consumer.push).catch(consumer.error);
+        _consume();
+      } else {
+        self.close();
+      }
+    });
+  })();
+  return pipe;
 };
 
 Stream.prototype.each = function(cb, done, failure) {
@@ -46,12 +69,18 @@ Stream.prototype.each = function(cb, done, failure) {
   });
 };
 
-function DefaultStream(nextFn) {
-  this.next = nextFn;
+function StreamWrapper(wrapped, nextFn) {
+  this.wrapped = wrapped;
+  this.next = _.bind(nextFn, this);
 };
-DefaultStream.prototype = _.create(Stream.prototype);
+StreamWrapper.prototype = _.create(Stream.prototype);
+
+StreamWrapper.prototype.close = function() {
+  this.wrapped.close();
+}
 
 function ListStream(items) {
+  Stream.call(this);
   this.index = 0;
   this.items = items;
 };
@@ -66,7 +95,82 @@ ListStream.prototype.next = function() {
   }
 };
 
+function Pipe() {
+  Stream.call(this);
+  this.nextResolve = null;
+  this.nextReject = null;
+
+  this.readyResolve = null;
+  this.readyReject = null;
+}
+Pipe.prototype = _.create(Stream.prototype);
+
+Pipe.prototype.wrapper = PipeWrapper;
+
+Pipe.prototype.ready = function() {
+  var self = this;
+  if (self.readyResolve !== null) {
+    throw new Error('Invalid state');
+  } else if (self.nextResolve !== null) {
+    return Promise.resolve({
+      push: self.nextResolve,
+      error: self.nextReject,
+      done: false
+    });
+    self.nextResolve = null;
+    self.nextReject = null;
+  } else {
+    return new Promise(function(resolve, reject) {
+      self.readyResolve = resolve;
+      self.readyReject = reject;
+    });
+  }
+};
+
+Pipe.prototype.next = function() {
+  var self = this;
+  if (self.nextResolve !== null) {
+    throw new Error('Invalid state');
+  } else if (self.readyResolve !== null) {
+    return new Promise(function(resolve, reject) {
+      self.readyResolve({
+        push: resolve,
+        error: reject,
+        done: false
+      });
+      self.readyResolve = null;
+      self.readyReject = null;
+    });
+  } else {
+    return new Promise(function(resolve, reject) {
+      self.nextResolve = resolve;
+      self.nextReject = reject;
+    });
+  }
+};
+
+function PipeWrapper(wrapped, nextFn, readyFn) {
+  this.wrapped = wrapped;
+  if (typeof nextFn === 'function') {
+      this.next = _.bind(nextFn, this);
+  }
+
+  if (typeof readyFn === 'function') {
+      this.ready = _.bind(readyFn, this);
+  }
+}
+PipeWrapper.prototype = _.create(Pipe.prototype);
+
+PipeWrapper.prototype.next = function() {
+  return this.wrapped.next();
+};
+
+PipeWrapper.prototype.ready = function() {
+  return this.wrapped.ready();
+};
+
 module.exports = {
   Stream: Stream,
-  ListStream: ListStream
+  ListStream: ListStream,
+  Pipe: Pipe,
 };
--- test.js
+++ test.js
@@ -1,11 +1,17 @@
 var $ = require('./index');
 
-var stream = new $.ListStream([3, 2, 6, 1, 5]);
-
-stream.map(function(i) {
+new $.ListStream([3, 2, 6, 1, 5]).map(function(i) {
   return i + 1;
 }).filter(function(i) {
   return i > 5;
-}).each(function(item) {
+}).pipe(new $.Pipe()).each(function(item) {
+  console.log(item);
+});
+
+new $.ListStream([3, 2, 6, 1, 5]).pipe(new $.Pipe().map(function(i) {
+  return i + 1;
+}).filter(function(i) {
+  return i > 5;
+})).each(function(item) {
   console.log(item);
 });

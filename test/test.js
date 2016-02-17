ffbdb2fb0a23af30125d970067b21555aa2c9285
Jean-Christophe Delmas <jc.delmas@gmail.com>
2016-02-17 23:15:20 +0100

    wip

--- test/test.js
+++ test/test.js
@@ -1,17 +1,18 @@
-var $ = require('./index');
+import {
+  Source,
+  Flow
+} from '../lib/index';
 
-new $.ListStream([3, 2, 6, 1, 5]).map(function(i) {
-  return i + 1;
-}).filter(function(i) {
-  return i > 5;
-}).pipe(new $.Pipe()).each(function(item) {
-  console.log(item);
-});
+Source.fromList([3, 2, 6, 1, 5])
+  .map(i => i + 1)
+  .filter(i => i > 5)
+  .via(new Flow())
+  .each(item => console.log(item))
+  .then(() => console.log('done'))
+  .catch(e => console.error(e.stack));
 
-new $.ListStream([3, 2, 6, 1, 5]).pipe(new $.Pipe().map(function(i) {
-  return i + 1;
-}).filter(function(i) {
-  return i > 5;
-})).each(function(item) {
-  console.log(item);
-});
+Source.fromList([3, 2, 6, 1, 5])
+  .via(Flow.map(i => i + 1).filter(i => i > 5))
+  .each(item => console.log(item))
+  .then(() => console.log('done'))
+  .catch(e => console.error(e.stack));

99c2dec0bffb6add04f33828e9e0ba5fabe72b37
Jean-Christophe Delmas <jc.delmas@zippyguide.com>
2016-02-19 17:49:40 +0100

    refactor: clean tests

--- test/test.js
+++ test/test.js
@@ -8,63 +8,63 @@ import {
 } from '../src/index';
 
 describe('Source', () => {
-  it('#from(list)', async () => {
+  it('from(list)', async () => {
     const result = await Source.from([1, 2, 3]).toList();
     result.should.be.eql([1, 2, 3]);
   });
-  it('#forEach', async () => {
+  it('forEach', async () => {
     const xs = [];
     await Source.from([1, 2, 3]).forEach(x => xs.push(x));
     xs.should.be.eql([1, 2, 3]);
   });
-  it('#to', async () => {
+  it('to', async () => {
     const result = await Source.from([1, 2, 3]).to(Sink.toList()).run();
     result.should.be.eql([1, 2, 3]);
   });
 });
 
 describe('FlowOps', () => {
-  it('#map', async () => {
+  it('map', async () => {
     const result = await Source.from([1, 2, 3]).map(x => x + 1).toList();
     result.should.be.eql([2, 3, 4]);
   });
-  it('#filter', async () => {
+  it('filter', async () => {
     const result = await Source.from([1, 2, 3, 4, 5]).filter(x => x > 2).toList();
     result.should.be.eql([3, 4, 5]);
   });
-  it('#scan', async () => {
+  it('scan', async () => {
     const result = await Source.from([1, 1, 1]).scan((acc, x) => acc + x, 0).toList();
     result.should.be.eql([1, 2, 3]);
   });
-  it('#mapConcat', async () => {
+  it('mapConcat', async () => {
     const result = await Source.from([1, 2, 3]).mapConcat(x => [x, x]).toList();
     result.should.be.eql([1, 1, 2, 2, 3, 3]);
   });
-  it('#grouped', async () => {
+  it('grouped', async () => {
     const result = await Source.from([1, 2, 3, 4, 5]).grouped(2).toList();
     result.should.be.eql([[1, 2], [3, 4], [5]]);
   });
-  it('#sliding', async () => {
+  it('sliding', async () => {
     const result = await Source.from([1, 2, 3, 4, 5]).sliding(3).toList();
     result.should.be.eql([[1, 2, 3], [2, 3, 4], [3, 4, 5]]);
   });
-  it('#sliding - 2', async () => {
+  it('sliding - 2', async () => {
     const result = await Source.from([1, 2, 3, 4]).sliding(3, 2).toList();
     result.should.be.eql([[1, 2, 3], [3, 4]]);
   });
-  it('#take - 1', async () => {
+  it('take - 1', async () => {
     const result = await Source.from([1, 2, 3, 4]).take(2).toList();
     result.should.be.eql([1, 2]);
   });
-  it('#take - 2', async () => {
+  it('take - 2', async () => {
     const result = await Source.from([1, 2]).take(4).toList();
     result.should.be.eql([1, 2]);
   });
-  it('#drop - 1', async () => {
+  it('drop - 1', async () => {
     const result = await Source.from([1, 2, 3, 4]).drop(2).toList();
     result.should.be.eql([3, 4]);
   });
-  it('#drop - 2', async () => {
+  it('drop - 2', async () => {
     const result = await Source.from([1, 2]).drop(4).toList();
     result.should.be.eql([]);
   });
@@ -89,7 +89,7 @@ describe('Flow', () => {
 });
 
 describe('Sink', () => {
-  it('#fold', async () => {
+  it('fold', async () => {
     const result = await Source.from([1, 2, 3]).reduce((acc, x) => x + acc, 0);
     result.should.be.eql(6);
   });

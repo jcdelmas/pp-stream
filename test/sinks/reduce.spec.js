c0517c30ff2364872a19094752d0815c422eb96a
Jean-Christophe Delmas <jc.delmas@gmail.com>
2017-08-25 22:02:21 +0200

    add side effect check on reduce sink

--- test/sinks/reduce.spec.js
+++ test/sinks/reduce.spec.js
@@ -2,11 +2,19 @@
 
 import 'babel-polyfill';
 import 'should';
-import { Source } from '../../src/index';
+import { Source, Sink } from '../../src/index';
 
 describe('reduce', () => {
   it('simple', async() => {
     const result = await Source.from([1, 2, 3]).reduce((acc, x) => x + acc, 0);
+    result.should.be.eql(6);
+  });
+
+  it('check side effects', async() => {
+    const sink = Sink.reduce((acc, x) => x + acc, 0);
+
+    await Source.from([1, 2, 3]).runWith(sink);
+    const result = await Source.from([1, 2, 3]).runWith(sink);
     result.should.be.eql(6);
   });
 });

dd08d115b67bd8e89a2f9b7b9ce74625a02a85bf
Jean-Christophe Delmas <jc.delmas@gmail.com>
2018-06-24 21:35:56 +0200

    simplify flatMapConcat implementation

--- src/flows/flat-map-concat.ts
+++ src/flows/flat-map-concat.ts
@@ -1,9 +1,10 @@
 import { _registerFlow, flow, FlowStage } from 'core/flow'
 import { SinkStage, sink } from 'core/sink'
 import { Source } from 'core/source'
+import { flatMapMerge } from './flat-map-merge'
 
 export function flatMapConcat<I, O>(fn: (x: I) => Source<O>) {
-  return flow(() => new FlatMapConcat<I, O>(fn));
+  return flatMapMerge(fn, 1)
 }
 
 declare module '../core/source' {
@@ -18,68 +19,4 @@ declare module '../core/flow' {
   }
 }
 
-_registerFlow('flatMapConcat', flatMapConcat);
-
-class FlatMapConcat<I, O> extends FlowStage<I, O> {
-  constructor(private readonly fn: (x: I) => Source<O>) {
-    super()
-  }
-
-  current?: SinkStage<O, void>
-  completePending: boolean = false;
-
-  onPush() {
-    const source = this.fn(this.grab());
-    this.current = new FlatMapSink<O>(this)
-    source.runWith(sink(() => this.current));
-  }
-
-  onPull() {
-    if (this.current) {
-      this.current.pullIfAllowed()
-    } else {
-      this.pull()
-    }
-  }
-
-  onCancel() {
-    if (this.current) {
-      this.current.cancel()
-    }
-    this.cancel();
-  }
-
-  onComplete() {
-    if (!this.current) {
-      this.complete()
-    } else {
-      this.completePending = true;
-    }
-  }
-}
-
-class FlatMapSink<I> extends SinkStage<I, void> {
-
-  constructor (private readonly parent: FlatMapConcat<any, I>) {
-    super()
-  }
-
-  onPush() {
-    this.parent.push(this.grab())
-  }
-
-  onComplete() {
-    this.parent.current = undefined
-    if (this.parent.completePending) {
-      this.parent.complete()
-    }
-  }
-
-  onError(e: any): void {
-    this.parent.error(e)
-  }
-
-  onStart() {
-    this.pull()
-  }
-}
+_registerFlow('flatMapConcat', flatMapConcat)
